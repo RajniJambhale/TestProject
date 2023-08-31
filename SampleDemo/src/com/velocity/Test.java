cfb72bb99b2c57f05fe514b59f93a61d54e1ec04
RajniJambhale <rajnijambhale49472@gmail.com>
2023-08-31 15:13:26 +0530

    arraylist code added

---
+++ SampleDemo/src/com/velocity/Test.java
@@ -0,0 +1,19 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add("pune");
+		al.add("delhi");
+		al.add("Mumbai");
+		al.add("Bangalore");
+		
+		System.out.println(al);
+
+	}
+
+}
